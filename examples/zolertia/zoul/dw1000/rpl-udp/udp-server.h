99a2b5715af65120adef49ac71f38c80d5116544
Thomas Lavend'Homme <lavendthomas@outlook.be>
2021-02-23 13:28:19 +0100

    Firmware for mobility : UDP Client

---
+++ examples/zolertia/zoul/dw1000/rpl-udp/udp-server.h
@@ -0,0 +1,24 @@
+#include "contiki.h"
+#include "net/mac/tsch/tsch.h"
+#include "net/mac/tsch/tsch-schedule.h" 
+
+extern const linkaddr_t node_1_address;
+extern const linkaddr_t node_2_address;
+extern const linkaddr_t node_3_address;
+extern const linkaddr_t node_4_address;
+extern const linkaddr_t node_5_address;
+extern const linkaddr_t node_6_address;
+extern const linkaddr_t node_7_address;
+extern const linkaddr_t node_8_address;
+extern const linkaddr_t node_9_address;
+extern const linkaddr_t node_10_address;
+extern const linkaddr_t node_11_address;
+extern const linkaddr_t node_12_address;
+extern const linkaddr_t node_13_address;
+extern const linkaddr_t node_14_address;
+extern const linkaddr_t node_15_address;
+extern const linkaddr_t node_16_address;
+extern const linkaddr_t node_17_address;
+extern const linkaddr_t * mac_neighborg_addr[17];
+
+void tsch_schedule_create_udp_server(void);

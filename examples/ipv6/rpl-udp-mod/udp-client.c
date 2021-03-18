45cf4f24d414d1844e99507a8a29c44cfdf574fb
Thomas Lavend'Homme <lavendthomas@outlook.be>
2021-03-18 16:22:18 +0100

    Try with TSCH

--- examples/ipv6/rpl-udp-mod/udp-client.c
+++ examples/ipv6/rpl-udp-mod/udp-client.c
@@ -43,6 +43,8 @@
 #include "dev/serial-line.h"
 #include "net/ipv6/uip-ds6-route.h"
 
+#include "examples/ipv6/rpl-udp-mod/udp-client.h"
+
 #define UDP_CLIENT_PORT 8765
 #define UDP_SERVER_PORT 5678
 
@@ -103,6 +105,8 @@ send_packet(void *ptr)
              reply == seq_id ? "GREEN" : "RED", uip_ds6_route_num_routes(), num_used);
   }
 #endif /* SERVER_REPLY */
+
+  rpl_print_neighbor_etx_list();
 
   seq_id++;
   PRINTF("DATA send to %d 'Hello %d'\n",
@@ -183,6 +187,9 @@ PROCESS_THREAD(udp_client_process, ev, data)
   PROCESS_PAUSE();
 
   set_global_address();
+
+  // Define the schedule
+  tsch_schedule_create_udp_client();
 
   PRINTF("UDP client process started nbr:%d routes:%d\n",
          NBR_TABLE_CONF_MAX_NEIGHBORS, UIP_CONF_MAX_ROUTES);
@@ -266,3 +273,91 @@ PROCESS_THREAD(udp_client_process, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+
+
+const linkaddr_t node_1_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x01 } };
+const linkaddr_t node_2_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x02 } };
+const linkaddr_t node_3_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x03 } };
+const linkaddr_t node_4_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x04 } };
+const linkaddr_t node_5_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x05 } };
+const linkaddr_t node_6_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x06 } };
+const linkaddr_t node_7_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x07 } };
+const linkaddr_t node_8_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x08 } };
+const linkaddr_t node_9_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x09 } };
+const linkaddr_t node_10_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0A } };
+const linkaddr_t node_11_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0B } };
+const linkaddr_t node_12_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0C } };
+const linkaddr_t node_13_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0D } };
+const linkaddr_t node_14_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0E } };
+const linkaddr_t node_15_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x0F } };
+const linkaddr_t node_16_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x10 } };
+const linkaddr_t node_17_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x11 } };
+const linkaddr_t node_m1_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x12 } };
+const linkaddr_t node_m2_address = { { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0X00, 0x13 } };
+
+const linkaddr_t * mac_neighborg_addr[] = { 
+  &node_1_address, 
+  &node_2_address, 
+  &node_3_address, 
+  &node_4_address, 
+  &node_5_address, 
+  &node_6_address, 
+  &node_7_address, 
+  &node_8_address, 
+  &node_9_address, 
+  &node_10_address, 
+  &node_11_address, 
+  &node_12_address, 
+  &node_13_address, 
+  &node_14_address, 
+  &node_15_address, 
+  &node_16_address, 
+  &node_17_address,
+  &node_m1_address,
+  &node_m2_address
+};
+
+
+/*---------------------------------------------------------------------------*/
+
+/* Ensure that the NODEID macro is correctly passed by the Makefile.
+   That's not the case for each target platform. */
+#ifndef NODEID
+#  error "Node ID not defined. Perhaps you need to tweak target Makefile."
+#endif /* NODEID */
+
+void tsch_schedule_create_udp_client(void)
+{
+  struct tsch_slotframe *sf_custom;
+
+  /* First, empty current schedule */
+  tsch_schedule_remove_all_slotframes();
+
+  /* Build schedule.
+   * We pick a slotframe length of TSCH_SCHEDULE_DEFAULT_LENGTH */
+  sf_custom = tsch_schedule_add_slotframe(0, 50);
+
+  const struct {
+    struct tsch_slotframe *slotframe;
+    uint8_t                link_options;
+    enum link_type         link_type;
+    const linkaddr_t      *address;
+    uint16_t               timeslot;
+    uint16_t               channel_offset;
+  } timeslots[] = {
+    { sf_custom, LINK_OPTION_TX | LINK_OPTION_RX | LINK_OPTION_SHARED | LINK_OPTION_TIME_KEEPING, LINK_TYPE_ADVERTISING, &tsch_broadcast_address, 0, 0 },
+#if NODEID == 0x01
+    { sf_custom, LINK_OPTION_TX, LINK_TYPE_NORMAL, &node_2_address, 2, 0 },
+    { sf_custom, LINK_OPTION_RX, LINK_TYPE_NORMAL, &node_2_address, 4, 0 },
+#elif NODEID == 0x02
+    { sf_custom, LINK_OPTION_RX, LINK_TYPE_NORMAL, &node_1_address, 2, 0 },
+    { sf_custom, LINK_OPTION_TX, LINK_TYPE_NORMAL, &node_1_address, 4, 0 },
+#else
+#  error "Unhandled NODEID for static schedule."
+#endif /* NODEID */
+    { 0 }
+  }, *l;
+
+  for(l = timeslots ; l->slotframe ; l++)
+    tsch_schedule_add_link(l->slotframe, l->link_options, l->link_type, l->address, l->timeslot, l->channel_offset);
+}
